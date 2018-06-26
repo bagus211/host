93798360e4a4c5eb171412ae00656386062bddbe
bagus211 <40594626+bagus211@users.noreply.github.com>
2018-06-26 22:38:44 +0700

    Update and rename index.md to index.php

---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+$a = unserialize(file_get_contents('http://www.geoplugin.net/php.gp?ip='.$_SERVER['REMOTE_ADDR']));
+$countrycode= $a['geoplugin_countryCode'];
+
+if ($countrycode=='AT')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=168757&s1=BAGUS' ) ;
+else if ($countrycode=='AU')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=129558&s1=BAGUS' ) ;
+else if ($countrycode=='BE')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=101955&s1=BAGUS' ) ;
+else if ($countrycode=='CA')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=165170&s1=bagus' ) ;
+else if ($countrycode=='CH')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=162873&s1=bagus' ) ;
+else if ($countrycode=='CZ')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=154826&s1=bagus' ) ;
+else if ($countrycode=='DE')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=172009&s1=bagus' ) ; 
+else if ($countrycode=='DK')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=150402&s1=bagus' ) ; 
+else if ($countrycode=='ES')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=123412&s1=bagus' ) ; 
+else if ($countrycode=='FI')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=152144&s1=bagus' ) ; 
+else if ($countrycode=='FR')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=166715&s1=bagus' ) ; 
+else if ($countrycode=='GB')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=167938&s1=bagus' ) ; 
+else if ($countrycode=='HK')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=141320&s1=bagus' ) ; 
+else if ($countrycode=='HU')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=169800&s1=bagus' ) ; 
+else if ($countrycode=='IE')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=116298&s1=bagus' ) ; 
+else if ($countrycode=='IT')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=175267&s1=bagus' ) ; 
+else if ($countrycode=='JP')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=152560&s1=bagus' ) ; 
+else if ($countrycode=='KR')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=117994&s1=bagus' ) ; 
+else if ($countrycode=='NL')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=168333&s1=bagus' ) ; 
+else if ($countrycode=='NO')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=154873&s1=bagus' ) ; 
+else if ($countrycode=='PL')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=116282&s1=bagus' ) ; 
+else if ($countrycode=='PT')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=164398&s1=bagus' ) ; 
+else if ($countrycode=='RU')
+    header( 'Location: http://cl-npltf.com/?a=64054&c=94892&s1=bagus' ) ; 
+else if ($countrycode=='SE')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=157889&s1=bagus' ) ; 
+else if ($countrycode=='SG')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=121873&s1=bagus' ) ; 
+else if ($countrycode=='TR')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=164633&s1=bagus' ) ; 
+else if ($countrycode=='TW')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=145855&s1=bagus' ) ; 
+else if ($countrycode=='NZ')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=167938&s1=bagus' ) ; 
+else if ($countrycode=='US')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=167980&s1=bagus' ) ; 
+else if ($countrycode=='ZA')
+    header( 'Location: https://securecloud-dt.com/?a=64054&c=166902&s1=bagus' ) ; 
+else if ($countrycode=='AR')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus ) ; 
+else if ($countrycode=='BO')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='CL')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='CO')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='EC')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=baguss' ) ; 
+else if ($countrycode=='SV')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='GQ')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='GT')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='HN')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='MX')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='NI')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='PA')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='PY')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='PE')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='UY')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=167642&s1=bagus' ) ; 
+else if ($countrycode=='VE')
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=164633&s1=bagus' ) ; 
+else if ($countrycode=='ID')
+    header( 'Location: http://FACEBOOK.COM' ) ; 
+else
+    header( 'Location: http://securecloud-dt.com/?a=64054&c=118847&s1=bagus' ) ;
+?>
